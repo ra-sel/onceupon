1324a1fd832bbdabf67dd6de041a1383396d7d3e
Md Rasel Ahmed <61163868+ra-sel@users.noreply.github.com>
2020-06-10 01:19:02 +0600

    Add files via upload

---
+++ precache-manifest.063cad3ff43b392a35aa7788921524f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9486075a9262496c8a4aa008f89391fc",
+    "url": "/onceupon/index.html"
+  },
+  {
+    "revision": "014b2a329964b95a9ce2",
+    "url": "/onceupon/static/css/main.0a315abb.chunk.css"
+  },
+  {
+    "revision": "70aeaea8e9d40482463c",
+    "url": "/onceupon/static/js/2.44e846c9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/onceupon/static/js/2.44e846c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "014b2a329964b95a9ce2",
+    "url": "/onceupon/static/js/main.436278d9.chunk.js"
+  },
+  {
+    "revision": "97153b5458ac221085f1",
+    "url": "/onceupon/static/js/runtime-main.4221e970.js"
+  }
+]);
